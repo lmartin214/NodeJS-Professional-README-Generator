89742b360844d05240b09b5d5d3ec8bb524a5000
lmartin214@yahoo.com <you@example.com>
2022-10-27 13:49:20 -0700

    imported packs, array of questions, functions for : userInput; write README & init app"

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,75 @@
-// TODO: Include packages needed for this application
+//Packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+//Created an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+      },
+      {
+        type: 'input',
+        name: 'title',
+        message: "What is your project's name?",
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'APACHE 2.0', 'Eclipse Public License 1.0', 'BSD 3', 'None'],
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What command should be run to install dependencies?',
+        default: 'npm i',
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'What command should be run to run tests?',
+        default: 'npm test',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'What does the user need to know about using the repo?',
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'What does the user need to know about contributing to the repo?',
+      },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//Function for users input to tobe returned as answers
+function userInput() {
+    return inquirer.prompt(questions);
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+//Created a function to write README file
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
-// Function call to initialize app
+//Created a function to initialize app
+async function init() {
+    let userAnswers = await userInput();
+    writeToFile((userAnswers.file), (generateMarkdown(userAnswers)))
+}
+
+//Function call to initialize app
 init();
