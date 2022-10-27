ba5a356060df9099e2d5caea4e907262c13ac6ab
lmartin214@yahoo.com <you@example.com>
2022-10-27 14:07:15 -0700

    markDown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,42 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const badgeLicense = data.license.split(" ").join("%20");
+  const urlLicense = data.license.split(" ").join("-");
 
+  return `# ${data.title}
+  
+  [![License: ${data.license}](https://img.shields.io/badge/License-${badgeLicense}-blue.svg)](https://opensource.org/licenses/${urlLicense})
+  ## Description
+  ${data.description}
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Description](#description)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## description
+  ${data.description}
+  ## License
+  [![License: ${data.license}](https://img.shields.io/badge/License-${badgeLicense}-blue.svg)](https://opensource.org/licenses/${urlLicense})
+  
+  This application is covered by ${data.license} license, available at:
+  https://opensource.org/licenses/${urlLicense}
+  ## Contributing
+  ${data.contributing}
+  ## Tests
+  ${data.tests}
+  ## Questions
+  If you have any questions please contact me at:
+   - GitHub account:https://github.com/${data.github}
+   - e-mail account: ${data.email}
+ 
 `;
 }
 
